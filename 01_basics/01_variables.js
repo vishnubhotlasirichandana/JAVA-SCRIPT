0eb576a517039d8a65af072b722da82742168217
Siri Chandana <sirichandanavishnubhotla@gmail.com>
2025-09-02 01:22:03 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,11 @@
+const accountId = 164188
+let accountEmail = "sirichandana@gmail.com"
+var accountPassword = "123456"
+accountCity = "guntur"
+console.log([accountId, accountEmail, accountPassword, accountCity]);
+accountEmail = "chandan@gmail.com"
+accountPassword = "63040"
+accountCity = "Avanigadda"
+let accountState;
+console.log([accountEmail, accountPassword, accountCity, accountState]);
+
