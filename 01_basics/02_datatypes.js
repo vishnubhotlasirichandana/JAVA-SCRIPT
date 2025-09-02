c7ba80541c051ee3cff69bb403d751340f5c26b0
Siri Chandana <sirichandanavishnubhotla@gmail.com>
2025-09-02 01:40:03 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,24 @@
+/*primitive data types
+number = -2^53 to +2^53
+bigInt => beyond the range of number 
+string=> ""
+boolean=> true/false
+symbol=> mostly discused in react
+null=>intentional absence of values 
+undefined=> declared but not assigned a value */
+let age = 19
+let name = "Siri Chandana Vishnubhotla"
+let inRelation = false 
+let anyXRelation = null
+let StateOfMind;
+console.log([age, name, inRelation, anyXRelation, StateOfMind]);
+console.log(typeof(anyXRelation));// null will represented as type of object 
+console.log(typeof(StateOfMind));//remaining all same as disscused datatype 
+console.log(typeof(name));
+console.log(typeof(age));
+
+
+
+
+
+
