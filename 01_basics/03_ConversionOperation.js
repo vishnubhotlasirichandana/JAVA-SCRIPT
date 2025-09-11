481d69d78aaa27f6e0fc77d0786a16a37434dfc7
Siri Chandana <sirichandanavishnubhotla@gmail.com>
2025-09-11 01:40:54 +0000

    comparsion of data types

---
+++ 01_basics/03_ConversionOperation.js
@@ -0,0 +1,16 @@
+let IsLoggedIn = 1
+let booleanIsLoggedIn = Boolean(IsLoggedIn)
+console.log(booleanIsLoggedIn);
+
+let score = "Siri"
+let valueInNumber = Number(score)
+console.log(valueInNumber);
+
+let SomeNummber = 33
+let StringNumber = String(SomeNummber)
+console.log(StringNumber);
+console.log(typeof StringNumber);
+
+//************** Operations ************ */
+
+
---
+++ 01_basics/04_Comparsions.js
@@ -0,0 +1,16 @@
+console.log(null == 0);//we avoid this comparsion(null&undefine)
+console.log(null > 0);
+console.log(null < 0);
+console.log(null >= 0);
+
+
+console.log("2" === 2);//in this "===" we strictly check with data type
+
+console.log(2>1);//direct comparsion(generally we do)
+console.log("2" > 1);//comparsion of different data types
+console.log("02" > 1);
+
+
+
+
+
