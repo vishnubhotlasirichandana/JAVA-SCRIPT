2f6f5ddc0d588a46a95835cc59683e75f7bf445c
Siri Chandana <sirichandanavishnubhotla@gmail.com>
2025-09-12 13:08:16 +0000

    Day-2 Operators

---
+++ Week1/Day2.js
@@ -0,0 +1,23 @@
+//Operators
+let a=10;
+let b = 3;
+let age = 20;
+let EmailID = true;
+console.log(" Arithmatic operator");
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(a/b);
+console.log(a%b);
+console.log("=====================");
+console.log(" Comapison operator")
+console.log(5<3);
+console.log(5>3);
+console.log("5" == 3);
+console.log(5 === "3");
+console.log(5 !== 3);
+console.log("=====================");
+console.log(" Logical operator")
+console.log(age > 18 && EmailID);
+console.log(age > 18 || !EmailID);
+console.log(!EmailID);
