64a074ca6b2531ec524ffa9207af14f24a9b2638
Siri Chandana <sirichandanavishnubhotla@gmail.com>
2025-09-12 14:41:34 +0000

    Day2-pratice

---
+++ Week1/Day2-pratice.js
@@ -0,0 +1,48 @@
+console.log("voting eligibility");
+console.log("====================");
+
+let age = 16;
+if(age>=18){
+    console.log("your eligible for voting");
+}
+else{
+    console.log("your not eligible for voting");
+}
+console.log("login Check");
+console.log("============");
+let username = "admin";
+let password = "1234";
+if(username == "admin" && password == "1234"){
+    console.log("login Success");
+}
+else{
+    console.log("login failed");
+}
+console.log("Grade Calculator");
+console.log("================");
+let marks=60;
+if(marks>=50){
+    console.log("Pass");
+    
+}
+else{
+    console.log("Fail");
+    
+}
+console.log("React-Style Example");
+console.log("===================");
+let isloggedIn = false;
+if(isloggedIn){
+    console.log("Welcome Back!");
+    
+}
+else{
+    console.log("Please log in");
+    
+}
+
+
+
+
+
+
