7201ef2b32c9ec54986c24592133c59728abd627
Siri Chandana <sirichandanavishnubhotla@gmail.com>
2025-12-10 07:46:43 +0000

    Number and Maths

---
+++ 01_basics/06_num_and_math.js
@@ -0,0 +1,18 @@
+const amount = 100.89732
+// console.log(amount);
+
+const balance = new Number(100)
+// console.log(balance);
+// console.log(balance.toFixed(2));
+// console.log(amount.toPrecision(5));
+const hundreds = 1000000
+// console.log(hundreds.toLocaleString('en-IN'));
+console.log(Math.random());//random always gives values between 0 and 1's
+console.log(Math.random()*10);//shift value to left and we added 1 to avoid 0 value when we do floor
+console.log(Math.floor(Math.random()*10)+1);//floor value means only gives least value and avoid point after value  
+const min = 10;
+const max = 20;
+console.log(Math.floor(Math.random() * (max - min + 1)) + min);
+
+
+
